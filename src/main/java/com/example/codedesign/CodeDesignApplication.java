c9736701687a57bd6fd89b764bd8ecbe99ef067f
diviner16 <github@gmail.com>
2023-09-30 14:47:58 +0800

    init

---
+++ src/main/java/com/example/codedesign/CodeDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.codedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDesignApplication.class, args);
+	}
+
+}
